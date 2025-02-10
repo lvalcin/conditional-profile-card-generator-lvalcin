ff89ce2110551a26d65adce751c036ffa8d19a0d
Lovnyth Valcin <lovnyth@gmail.com>
2025-02-10 02:44:51 +0000

    city and country input varibale updated within render function

--- src/js/app.js
+++ src/js/app.js
@@ -35,7 +35,7 @@ function render(variables = {}) {
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${variables.name} ${variables.lastName}</h1>
           <h2>${variables.role}</h2>
-          <h3>Miami, USA</h3>
+          <h3>${variables.city},${variables.country}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
